e6e797b7e6a653907bb871497af065995cebf3a7
Sammm <ntdsam@gmail.com>
2024-07-21 00:06:19 +0700

    add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
 import { navLinks } from "../constants";
-import { logo, menu, close } from "../assets";
+import { sam, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -23,7 +23,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <img src={sam} alt="logo" className="w-9 h-9 object-contain" />
         </Link>
       </div>
     </nav>
