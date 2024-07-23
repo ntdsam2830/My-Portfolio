5be2fc28fcf8203806a4e6fc28803aa6d2d6306d
ntdsam2830 <ntdsam@gmail.com>
2024-07-23 20:50:23 +0700

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,18 +23,26 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={sam} alt="logo" className="w-9 h-9 object-contain" />
+          <img src={sam} alt="logo" className="w-13 h-13 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer">
             Sam <span className="sm:block hidden">| Developer</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
-          {navLinks.map((Link) => (
-            <li>
-              <a href={`#${Link.id}`}>{Link.title}</a>
+          {navLinks.map((link) => (
+            <li  key={link.id}
+            className={`${
+              active === link.title ? "text-white" : "text-secondary"
+            } hover:text-white text-[18px] font-medium cursor-pointer`}
+            onClick={() => setActive(link.title)}>
+              <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
         </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img src={toggle ? close:menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer" onClick={()=>setToggle(!toggle)}/>
+        </div>
       </div>
     </nav>
   );
