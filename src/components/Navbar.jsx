cc9665a126de69cca46b16b8f5299fb26107eab2
Sammm <ntdsam@gmail.com>
2024-07-24 07:32:13 +0700

    fix logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,25 +23,38 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={sam} alt="logo" className="w-13 h-13 object-contain" />
+          <img src={sam} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer">
             Sam <span className="sm:block hidden">| Developer</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
-            <li  key={link.id}
-            className={`${
-              active === link.title ? "text-white" : "text-secondary"
-            } hover:text-white text-[18px] font-medium cursor-pointer`}
-            onClick={() => setActive(link.title)}>
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
         </ul>
 
         <div className="sm:hidden flex flex-1 justify-end items-center">
-          <img src={toggle ? close:menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer" onClick={()=>setToggle(!toggle)}/>
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain"
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          ></div>
         </div>
       </div>
     </nav>
