de596ccc575d3e9284b7092d43da44d384869225
Sammm <ntdsam@gmail.com>
2024-07-24 23:35:12 +0700

    Create loader

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
-export default Computers
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+    </mesh>
+  );
+};
+
+export default Computers;
