eeafccb9b2832bddb8586420b313681012199890
Sammm <ntdsam@gmail.com>
2024-07-30 14:25:55 +0700

    delete warning

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
