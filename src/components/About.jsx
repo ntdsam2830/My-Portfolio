bd4e255bcb249189453def5a7f8521ead856b397
Sammm <ntdsam@gmail.com>
2024-07-30 14:31:17 +0700

    disable warning

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-unknown-property */
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
